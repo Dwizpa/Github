8ca22b758ec74ac46e1771ddb84cdb61bc8793ee
Dwizpa <54273907+Dwizpa@users.noreply.github.com>
2021-10-26 23:36:25 +0700

    Adding EndlessRunning-Dilo Project
    Adding EndlessRunning-Dilo Project
    Without added another feature

---
+++ 2D Endless Runner - DILO/Assets/Scripts/CharacterMoveController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    public float moveAccel;
+    public float maxSpeed;
+    public float jumpAccel;
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
+    [Header("Scoring")]
+    public ScoreController score;
+    public float scoringRatio;
+    private float lastPositionX;
+
+    [Header("GameOver")]
+    public GameObject gameOverScreen;
+    public float fallPositionY;
+
+    [Header("Camera")]
+    public CameraMoveController gameCamera;
+
+    private Animator anim;
+    private bool isJumping;
+    private bool isOnGround;
+    private CharacterSoundController sound;
+    private Rigidbody2D rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sound = GetComponent<CharacterSoundController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //read input
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (isOnGround)
+            {
+                isJumping = true;
+                sound.PlayJump();
+            }
+        }
+
+        //change animation
+        anim.SetBool("isOnGround", isOnGround);
+
+        //calculate score
+        int distancePassed = Mathf.FloorToInt(transform.position.x - lastPositionX);
+        int scoreIncrement = Mathf.FloorToInt(distancePassed / scoringRatio);
+
+        if(scoreIncrement > 0 )
+        {
+            score.IncreaseCurrentScore(scoreIncrement);
+            lastPositionX += distancePassed;
+        }
+
+        //game over
+        if(transform.position.y < fallPositionY)
+        {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        //set high score
+        score.FinishScoring();
+
+        //stop camera movement
+        gameCamera.enabled = false;
+
+        //show gameover
+        gameOverScreen.SetActive(true);
+
+        //disable this too
+        this.enabled = false;
+    }
+    void FixedUpdate()
+    {
+        //raycast ground
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundRaycastDistance, groundLayerMask);
+        if(hit)
+        {
+            if(!isOnGround && rig.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+
+        //calculate velocity vector
+        Vector2 velocityVector = rig.velocity;
+
+        if(isJumping)
+        {
+            velocityVector.y += jumpAccel;
+            isJumping = false;
+        }
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+        rig.velocity = velocityVector;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down * groundRaycastDistance), Color.white);
+    }
+}
---
+++ 2D Endless Runner - DILO/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    private int currentScore = 0;
+
+    [Header("Score Highlight")]
+    public int scoreHighlight;
+    public CharacterSoundController sound;
+
+    private int lastScoreHighlight = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //reset
+        currentScore = 0;
+        lastScoreHighlight = 0;
+    }
+
+    public float GetCurrentScore()
+    {
+        return currentScore;
+    }
+
+    public void IncreaseCurrentScore(int increment)
+    {
+        currentScore += increment;
+
+        if(currentScore - lastScoreHighlight > scoreHighlight)
+        {
+            sound.PlayScoreHighlight();
+            lastScoreHighlight += scoreHighlight;
+        }
+    }
+
+    public void FinishScoring()
+    {
+        //set high score
+        if(currentScore > ScoreData.highScore)
+        {
+            ScoreData.highScore = currentScore;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
