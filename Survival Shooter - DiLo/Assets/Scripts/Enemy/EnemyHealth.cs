dc4edfeabb854e567d751fdb19944e86fff839d6
Dwizpa <54273907+Dwizpa@users.noreply.github.com>
2021-11-24 01:14:21 +0700

    Adding Survival Shooter - DiLo

---
+++ Survival Shooter - DiLo/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public float sinkSpeed = 2.5f;
+    public int scoreValue = 10;
+    public AudioClip deathClip;
+
+
+    Animator anim;
+    AudioSource enemyAudio;
+    ParticleSystem hitParticles;
+    CapsuleCollider capsuleCollider;
+    bool isDead;
+    bool isSinking;
+
+
+    void Awake ()
+    {
+        //Mendapatkan reference komponen
+        anim = GetComponent <Animator> ();
+        enemyAudio = GetComponent <AudioSource> ();
+        hitParticles = GetComponentInChildren <ParticleSystem> ();
+        capsuleCollider = GetComponent <CapsuleCollider> ();
+
+        //Set current Health
+        currentHealth = startingHealth;
+    }
+
+
+    void Update ()
+    {
+        //Check jika sinking
+        if (isSinking)
+        {
+            //memindahkan object kebawah
+            transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
+    }
+
+
+    public void TakeDamage (int amount, Vector3 hitPoint)
+    {
+        //Check jika dead
+        if (isDead)
+            return;
+
+        //play audio
+        enemyAudio.Play ();
+
+        //kurangi health
+        currentHealth -= amount;
+
+        //ganti posisi particle
+        hitParticles.transform.position = hitPoint;
+        //Play particle system
+        hitParticles.Play();
+
+        //Dead jika health <= 0
+        if (currentHealth <= 0)
+        {
+            Death ();
+        }
+    }
+
+
+    void Death ()
+    {
+        //set isdead
+        isDead = true;
+
+        //setcapcollider ke trigger
+        capsuleCollider.isTrigger = true;
+
+        //trigger play animation
+        anim.SetTrigger ("Dead");
+
+        //play sound dead
+        enemyAudio.clip = deathClip;
+        enemyAudio.Play ();
+    }
+
+
+    public void StartSinking ()
+    {
+        //disable navmesh componene
+        GetComponent<UnityEngine.AI.NavMeshAgent> ().enabled = false;
+        //set rigidbody ke kinematic
+        GetComponent<Rigidbody> ().isKinematic = true;
+        isSinking = true;
+        ScoreManager.score += scoreValue;
+        Destroy (gameObject, 2f);
+    }
+}
---
+++ Survival Shooter - DiLo/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRB;
+    int floorMask;
+    float camRayLength = 100f;
+
+    private void Awake()
+    {
+        //Mendapatkan nilai mask dari layer yang bernama Floor
+        floorMask = LayerMask.GetMask("Floor");
+
+        //Mendapatkan komponen Animator
+        anim = GetComponent<Animator>();
+
+        //Mendapatkan komponen Rigidbody
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        //Mendapatkan nilai input horizontal (-1,0,1)
+        float h = Input.GetAxisRaw("Horizontal");
+
+        //Mendapatkan nilai input vertical (-1,0,1)
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h,v);
+        Turning();
+        Animating(h, v);
+    }
+
+    public void Animating(float h, float v)
+    {
+        bool isWalking = h != 0f || v != 0f;
+        anim.SetBool("isWalking", isWalking);
+    }
+
+    private void Turning()
+    {
+        //Buat Ray dari posisi mouse di layar
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //Buat raycast untuk flootHit
+        RaycastHit floorHit;
+
+        //Lakukan raycast
+        if(Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            //Mendapatkan vector dari posisi player dan posisi floorHit
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            //Mendaptkan look rotation baru ke hit position
+            Quaternion newRot = Quaternion.LookRotation(playerToMouse);
+
+            playerRB.MoveRotation(newRot);
+        }
+    }
+
+    //Method player dapat berjalan
+    public void Move(float h, float v)
+    {
+        //Set nilai x dan y
+        movement.Set(h, 0f, v);
+
+        //Menormalisasi nilai vector agar total panjang dari vector adalah 1
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        //Move to position
+        playerRB.MovePosition(transform.position + movement);
+    }
+}
